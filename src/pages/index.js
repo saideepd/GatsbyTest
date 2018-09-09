c29689689bf87607295bd60783b4d4d0b3d70b9a
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2018-09-10 00:56:20 +0530

    the first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <h2>I'm Saideep Dicholkar</h2>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>This is a site which I have created using Gatsby.</p>
+    <p>And I discovered just now that Gatsby uses React. The code I wrote is in HTML.</p>
+    <p>Everything seems familiar</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
